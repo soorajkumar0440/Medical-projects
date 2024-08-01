244bcf3ea9a1949363dbaade640a748f104c948c
soorajkumar0440 <soorajkumar09770@gmail.com>
2024-08-02 02:38:38 +0500

    updated header link

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -21,7 +21,7 @@ const Header = () => {
           <ul>
             <a href="/"><li>Home</li></a>
             <a href="/about-us"><li>About</li></a>
-            <a href=""><li>Product<svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="size-6">
+            <li>Product<svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="size-6">
               <path stroke-linecap="round" stroke-linejoin="round" d="m19.5 8.25-7.5 7.5-7.5-7.5" />
             </svg>
               <div className='dropdown'>
@@ -30,7 +30,6 @@ const Header = () => {
                 <a href="/pharma"><li>pharmaceuticals</li></a>
               </div>
             </li>
-            </a>
             <a href="/distribution"><li>Distribution</li></a>
             <a href="/career"><li>Careers</li></a>
             <a href="/contact"><li>Contact</li></a>
